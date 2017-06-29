396d75fca1fdde57d13552fd2d5c48bf439c321f
KouLouYiMaSi <846137167@qq.com>
2017-06-29 23:43:51 +0800

    Update article.js

--- js/article.js
+++ js/article.js
@@ -7,7 +7,7 @@ $(function(){
 				var str="";
 				for (var i = 1; i < results.length; i++) {
 					str += "<tr><td>"+i+"</td>"+"<td>"+results[i].get("title")+"</td>"
-					+"<td>"+results[i].get("updatedAt")+"</td>"
+					+"<td>"+results[i].get("updatedAt").Format("yyyy-MM-dd HH:mm:ss")+"</td>"
 					"</tr>";
 				}
 				$("#ahead").attr("hidden",false);
@@ -18,3 +18,18 @@ $(function(){
 			}
 		});
 });
+Date.prototype.Format = function (fmt) { //author: meizz   
+    var o = {  
+        "M+": this.getMonth() + 1, //月份   
+        "d+": this.getDate(), //日   
+        "h+": this.getHours(), //小时   
+        "m+": this.getMinutes(), //分   
+        "s+": this.getSeconds(), //秒   
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度   
+        "S": this.getMilliseconds() //毫秒   
+    };  
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));  
+    for (var k in o)  
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));  
+    return fmt;  
+}  
